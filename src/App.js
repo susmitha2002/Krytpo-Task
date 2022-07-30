9254c7758efbe93a9bb04ebb3fe36410ba4d5858
susmitha <73429559+susmitha2002@users.noreply.github.com>
2022-07-30 07:16:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Showproducts from './components/Showproducts';
+import Trialcards from './components/Trialcards';
+
+function App() {
+  return (
+    <div className="App">
+      <Showproducts></Showproducts>
+      {/* <Trialcards></Trialcards> */}
+      {/* hii */}
+    </div>
+  );
+}
+
+export default App;
